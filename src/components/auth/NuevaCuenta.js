8daae0ef503f9d65631d9dda74c5a47ef599fa49
jimmy404 <bruno_pier@hotmail.com>
2020-10-03 13:43:25 -0300

    Adds value name for input form NuevaCuenta

--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,8 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+  //state para iniciar sesion
+  const [ usuario, guardarUsuario ] = useState({
+    email: '',
+    password: ''
+  });
+
+  //extraer de usuario
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name] : e.target.value
+    })
+  }
+
+  //cuando el usuario quiere iniciar sesion
+  const onSubmit = e => {
+    e.preventDefault();
+
+    //validar que no haya campos vacios
+
+    //pasarlo al action
+
+  }
+
   return(
-    <h1>Desde NuevaCuenta</h1>
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Crea una cuenta</h1>
+        <form
+          onSubmit={onSubmit}
+        >
+          <div className="campo-form">
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              name="nombre"
+              value={email}
+              placeholder="Tu Nombre"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Tu E-mail"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Tu Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="confirmar">Confirmar Password</label>
+            <input
+              type="password"
+              id="confirmar"
+              name="confirmar"
+              value={password}
+              placeholder="Repite tu Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input type="submit" className="btn btn-primario btn-block" value="registrar"/>
+          </div>
+
+        </form>
+        <Link to={'/nueva-cuenta'} className="enlace-cuenta">Obtener Cuenta</Link>
+      </div>
+    </div>
   );
 };
 
