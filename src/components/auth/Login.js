5ebb4f3b6f35ee005c4321e5aedda9c6754bb51e
jimmy404 <bruno_pier@hotmail.com>
2020-10-03 13:23:19 -0300

    Adds login code - form - styles

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
 const Login = () => {
+
+  const onChange = () => {}
+
   return(
-    <h1>Desde Login</h1>
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar sesion</h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu E-mail"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="campo-form">
+            <input type="submit" className="btn btn-primario btn-block"></input>
+          </div>
+
+        </form>
+      </div>
+    </div>
   );
 };
 
