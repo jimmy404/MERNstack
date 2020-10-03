ed542a86e7580341ab2fec928774e1db2be99dd7
jimmy404 <bruno_pier@hotmail.com>
2020-10-03 13:43:10 -0300

    Adds value name for input form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -61,7 +61,8 @@ const Login = () => {
           </div>
 
           <div className="campo-form">
-            <input type="submit" className="btn btn-primario btn-block"></input>
+            <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesion"
+            />
           </div>
 
         </form>
