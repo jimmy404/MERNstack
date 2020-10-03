cd826e96896a2badbebcdaed64fb39af7014b7e4
jimmy404 <bruno_pier@hotmail.com>
2020-10-03 17:13:07 -0300

    Adds task list

---
+++ src/components/tareas/ListadoTareas.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+
+  const tareasProyecto = [
+    { nombre: 'Elegir plataforma', estado: true },
+    { nombre: 'Elegir colores', estado: true },
+    { nombre: 'Elegir pagos', estado: false },
+    { nombre: 'Elegir antivirus', estado: true }
+  ];
+
+  return(
+    <Fragment>
+      <h2>Proyecto: Tienda Virtual</h2>
+      <ul className="listado-tareas">
+        {tareasProyecto.length === 0
+          ? (<li className="tarea"><p>No hay tareas</p></li>)
+          : tareasProyecto.map(tarea => (
+            <Tarea
+              tarea={tarea}
+            />
+          ))
+        }
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ListadoTareas;
---
+++ src/components/tareas/Tarea.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Tarea = () => {
+  return(
+    <h2>Desde Tarea</h2>
+  );
+}
+
+export default Tarea;
