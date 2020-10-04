e305c284029327ef623c8acb4e0b939247c1e00a
jimmy404 <bruno_pier@hotmail.com>
2020-10-04 12:38:05 -0300

    Adds code to hide the form when there are no products

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext'
+
 
 const FormTarea = () => {
-  return(
+
+    //extraer si un proyecto esta activo
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    //si no hay proyecto seleccionado
+    if(!proyecto) return null;
+
+    //array destructuring para extraer proyecto actual
+    const [proyectoActual] = proyecto;
+
+    return(
     <div className="formulario">
       <form>
         <div className="contenedor-input">
