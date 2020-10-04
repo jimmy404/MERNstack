8fd9ed5dfb2b674b9eb74463c7fc537c47335e53
jimmy404 <bruno_pier@hotmail.com>
2020-10-04 12:34:30 -0300

    Code to display the name of a product from the list

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext'
 import Tarea from './Tarea';
 
 const ListadoTareas = () => {
+
+    //obtener state de proyectos
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    //si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>
+
+    //array destructuring para extraer proyecto actual
+    const [proyectoActual] = proyecto;
 
   const tareasProyecto = [
     { nombre: 'Elegir plataforma', estado: true },
@@ -12,7 +23,7 @@ const ListadoTareas = () => {
 
   return(
     <Fragment>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0
           ? (<li className="tarea"><p>No hay tareas</p></li>)
