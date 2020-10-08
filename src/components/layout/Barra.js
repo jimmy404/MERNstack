e6f8259b6a48ac4da7d7d84858c59e6ef45b213f
jimmy404 <bruno_pier@hotmail.com>
2020-10-08 11:19:16 -0300

    Code to protect routes in case of not being logged in

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -5,7 +5,7 @@ const Barra = () => {
 
   // extraer informacion de auth
   const authContext = useContext(AuthContext);
-  const { usuario, usuarioAutenticado } = authContext;
+  const { usuario, usuarioAutenticado, cerrarSesion } = authContext;
 
   useEffect(() => {
     usuarioAutenticado();
@@ -17,7 +17,10 @@ const Barra = () => {
     <header className="app-header">
       {usuario ? <p className="nombre-usuario">Hola <span>{usuario.nombre}</span></p> : null}
       <nav className="nav-principal">
-      <a href="#!">Cerrar Sesion</a>
+      <button
+        className="btn btn-blank cerrar-sesion"
+        onClick={() => cerrarSesion()}
+        >Cerrar Sesion</button>
     </nav>
     </header>
   );
